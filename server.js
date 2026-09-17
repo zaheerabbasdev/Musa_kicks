4de8d9b9399dbedecc623d549df8a63782c07eb9
ripleyz <ap.eagletech@gmail.com>
2026-09-17 13:09:45 +0500

    deploy

---
+++ server.js
@@ -0,0 +1,12 @@
+const { createServer } = require("node:http");
+const next = require("next");
+
+const port = process.env.PORT || 3000;
+const app = next({ dev: false });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => handle(req, res)).listen(port, () => {
+    console.log(`Musa Kicks server ready on port ${port}`);
+  });
+});
